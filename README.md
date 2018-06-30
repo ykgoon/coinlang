f52e8911bec57de0507ebe5e9f65d0364509d552
Y.K. Goon <ykgoon@gmail.com>
2018-06-30 18:15:20 +0800

    Add emoji component

--- README.md
+++ README.md
@@ -4,4 +4,22 @@ Crowdsourced ideograph-based visual **human** language.
 
 In plain-speak: this is emoji built for serious convos.
 
-## Why?
+## Why
+
+## Develop with docker
+
+Requirements:
+
+- docker >= 18.0
+- docker-compose >= 1.19
+
+`./dev.sh` to go into node container.
+
+### Start development server
+
+`yarn start`
+
+### Produce production-build
+
+- `yarn build`
+- Compiled files found in `./app/build`.
--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Input, Layout, Row, Col } from 'antd';
+import { Picker } from 'emoji-mart';
 import 'antd/dist/antd.css';
+import 'emoji-mart/css/emoji-mart.css';
 import './App.css';
 
 const { TextArea } = Input;
@@ -21,6 +23,7 @@ class App extends Component {
                 placeholder="Write your core point"
                 autosize={{ minRows: 3, maxRows: 7 }}
                 />
+              <Picker set='emojione' title='Coin Language'/>
             </Col>
             <Col span={4}></Col>
           </Row>
